170d6d64973d9476ce9bd3350619be0404550fbb
guilhermefrag <guilhermefragnani@icloud.com>
2023-02-28 17:23:34 -0300

    itens fixos na lista

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,23 +1,44 @@
 import React, { Component } from "react";
 import "./Main.css";
+import { FaPlus } from "react-icons/fa";
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+
 class Main extends Component {
   state = {
     newTask: '',
+    tasks: [
+      'Fazer Café',
+      'Estudar React',
+      'Beber Água'
+    ]
   }
-  handleChange(e: React.ChangeEvent<HTMLInputElement>){
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
     this.setState({newTask: e.target.value});
   }
 
   render() {
-    const { newTask } = this.state;
+    const { newTask, tasks } = this.state;
     return (
       <div className="main">
-        <h1>Task List { newTask }</h1>
+        <h1>Task List</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Submit</button>
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={newTask}/>
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+        <ul className="tarefas">
+          {tasks.map(task => (
+            <li key={task}>
+              {task}
+              <div>
+              <FaEdit className="edit"/>
+              <FaWindowClose className="delete"/>
+              </div>
+              </li>
+          ))}
+        </ul>
       </div>
     );
   }
