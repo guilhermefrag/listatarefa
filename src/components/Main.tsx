b6767d5812e3ed3052697c73f3aae46bf5680b87
guilhermefrag <guilhermefragnani@icloud.com>
2023-02-28 16:12:26 -0300

    criacao ibiruba finalizada

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
-
+import "./Main.css";
 class Main extends Component {
+  state = {
+    newTask: '',
+  }
+  handleChange(e: React.ChangeEvent<HTMLInputElement>){
+    this.setState({newTask: e.target.value});
+  }
+
   render() {
+    const { newTask } = this.state;
     return (
-      <>
-        <h1>Hello World!</h1>
-        <p>Lorem</p>
-      </>
+      <div className="main">
+        <h1>Task List { newTask }</h1>
+
+        <form action="#">
+          <input onChange={this.handleChange} type="text" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     );
   }
 }
